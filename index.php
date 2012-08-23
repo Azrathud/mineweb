54a353614b4249b698850e5c131414d4a8022be7
Azrathud <azrathud@gmail.com>
2012-08-23 11:05:21 -0600

    add large tweaks. Almost finished html template end

--- index.php
+++ index.php
@@ -8,45 +8,52 @@
     </head>
     <body>
         <div id="wrapper">
-            <header>
+            <div id="header">
                 <h1> Azrathud Minecraft Server </h1>
                 <div id="logo"><a href="#"></a></div>
-            </header>
-            <nav>
-                <ul>
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="forums.php">Forums</a></li>
-                    <li><a href="news.php">Updates</a></li>
-                    <li><a href="server.azrathud.com:8123">Live Map</a></li>
-                    <li><a href="rules.php">Rules</a></li>
-                    <li><a href="donate.php">Donate</a></li>
-                </ul>
-            </nav>
-            <section id="main-wrapper">
-                <h1>Updates</h1>
-                <div id="main-body">
-                    <div class="content">
-                    &#060;dynamic-news-here&#062;
+            </div>
+            <div id="left-wrapper">
+                <div id="nav">
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="how-to-join.php">How to Join</a></li>
+                        <li><a href="rules.php">Rules</a></li>
+                        <li><a href="plugins.php">Plugins</a>
+                        <li><a href="command-list.php">Command List</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="screenshots.php">Screenshots</a></li>
+                        <li><a href="server.azrathud.com:8123">Live Map</a></li>
+                        <li><a href="forum.php">Forum</a></li>
+                        <li><a href="donate.php">Donate</a></li>
+                        <li><a href="staff.php">Staff</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+                <section id="main-wrapper">
+                    <h1>Updates</h1>
+                    <div id="main-body">
+                        <div class="content">
+                            <h2>sub header</h2>
+                            <p>&#060;dynamic-news-here&#062;</p>
+                        </div>
                     </div>
-                </div>
-                <div id="more-link">
-                    <a href="news.html">More</a>
-                </div>
-            </section>
-            <section id="sidebar">
+                </section>
+            </div>
+            <div id="sidebar">
                     <div id="status">
                         <h1>Server Status</h1>
                         <div class="content">&#060;dynamic-status-here&#062;</div>
                     </div>
                     <div id="about">
-                    	<h1>About</h1>
-                        <p class="content">A genuinely awesome, non-restrictive minecraft server. The only goal this server has is to create a fun, survival, semi-vanilla environment in which for people to play.</p>
+                        <h1>About</h1>
+                        <div class="content">A genuinely awesome, non-restrictive minecraft server. The only goal this server has is to create a fun, survival, semi-vanilla environment in which for people to play.</p>
                     </div>
                     <div id="starred-threads">
-                    	<h1>Starred Threads</h1>
+                        <h1>Starred Threads</h1>
+                        <div class="content">&#060;dynamic-content-here&#062;</div>
                     </div>
-                    <div class="content">&#060;dynamic-content-here&#062;</div>
-            </section>
+            </div>
         </div>
+    <script type="text/javascript" src="template.js"></script>
     </body>
 </html>
