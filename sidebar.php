60fd184317d75fbf72d1388f283eaf54d6d45d84
Azrathud <azrathud@gmail.com>
2012-11-02 22:03:22 -0600

    remove the sidebar from any page if all modules are turned off.

--- sidebar.php
+++ sidebar.php
@@ -1,15 +1,38 @@
 <?php
 class Sidebar {
     var $server_status;
+    var $modules_state = array();
     function __construct($server_status=false) {
+        #
+        # The Sidebar is disabled until any module found enabled
+        $this->sidebar_enabled = false;
+
         $this->server_status = $server_status;
+        array_push($this->modules_state, $server_status);
+        foreach($this->modules_state as $state)
+        {
+            if($state) {
+                $this->sidebar_enabled = true;
+            }
+        }
+
     }
 
     function output() {
-        $final_output = "";
+        if(! $this->sidebar_enabled) {
+            return "";
+        }
+        $final_output = <<<EOT
+<div id="sidebar" class="box">
+EOT;
+
         if($this->server_status) {
             $final_output .= $this->get_server_status();
         }
+
+    $final_output .= <<<EOT
+</div>
+EOT;
         return $final_output;
     }
 
