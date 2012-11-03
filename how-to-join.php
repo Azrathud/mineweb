311b0871384a86b223597beddb85814b49ca2b8a
Azrathud <azrathud@gmail.com>
2012-11-02 21:59:34 -0600

    clarify how to join server

--- how-to-join.php
+++ how-to-join.php
@@ -9,7 +9,7 @@ $template = new Template($sidebar);
 $template->content = <<<EOT
 <h1>How to join</h1>
 <div class="content">
-<p>Simply type <tt>server.azrathud.com</tt> into your minecraft address bar. This server is public for anyone to join.</p>
+<p>Simply type <tt>server.azrathud.com</tt> into your minecraft server address bar. This server is public for anyone to join and strike the earth right away.</p>
 </div>
 EOT;
 
